287cdbf19b5ba1686d95f607c1bf73ede767ba48
billyx86 <billymk777@gmail.com>
2023-02-07 13:16:18 +0000

    Complete /05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,21 @@
-const sumAll = function() {
+const sumAll = function(...args) {
+
+    console.log(args);
+
+    for(let i = 0; i < args.length; i++) {
+        if(typeof args[i] !== 'number') {
+            return("ERROR");
+        }
+    }
+
+    let answer = 0;
+    for(let i = Math.min(...args); i < Math.max(...args) + 1; i++) {
+        if(i < 0) {
+            return("ERROR");
+        }
+        answer += i;
+    }
+    return(answer);
 
 };
 
