37d717c8f097af0dd9baee0c2abab99f85ef18ad
billyx86 <billymk777@gmail.com>
2023-02-07 12:38:25 +0000

    Complete /04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    const array = args[0];
+    const newArray = [];
+    array.forEach((item) => {
+        if(!args.includes(item)) {
+            newArray.push(item);
+        }
+    })
+    return(newArray);
+}
 
-};
+/* const removeFromArray = function(array, index) {
+    const indexArray = [index];
+    for(let i = 0; i < indexArray.length; i++) {
+        if(array.includes(indexArray[i])) {
+            array.splice(array.indexOf(indexArray[i]), 1);
+        }
+    }
+    return(array);
+}; */
 
 // Do not edit below this line
 module.exports = removeFromArray;
