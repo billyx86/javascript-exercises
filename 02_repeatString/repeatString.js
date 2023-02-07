286c9031080f2ef1254e01224d4aa398ffcc9805
billyx86 <billymk777@gmail.com>
2023-02-07 11:27:29 +0000

    Complete /02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+let strArray = [];
 
+const repeatString = function(string, num) {
+    if(num >= 0) {
+        for(let i = 0; i < num; i++) {
+            strArray.push(string);
+        }
+        return(strArray.join(""));
+    } else {
+        return("ERROR")
+    }
 };
 
 // Do not edit below this line
