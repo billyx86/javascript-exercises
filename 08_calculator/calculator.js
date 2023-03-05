c4e3d268ed0f6114f40afd2bc208fe777b9e5b54
billyx86 <billymk777@gmail.com>
2023-03-05 18:04:39 +0000

    Complete 08_calculator add and subtraction functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,13 +1,13 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  c = a - b;
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
 };
 
 const multiply = function() {
