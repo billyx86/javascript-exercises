54ee211ff959d1480bb85527bb27932d3ef23ce8
billyx86 <billymk777@gmail.com>
2023-03-06 20:30:31 +0000

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    str = str.replace(/\s/g,'')
+    if((str.split("").reverse().join("")).toLowerCase() === str.toLowerCase()) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
