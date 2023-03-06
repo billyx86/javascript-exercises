2d60f66e655788749c6b50ee5815ec6b9a8b1281
billyx86 <billymk777@gmail.com>
2023-03-06 20:15:24 +0000

    Complete factorial function of 08_calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -25,8 +25,16 @@ const power = function(a, b) {
 	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	if(number === 0) {
+    return 1;
+  } else {
+    let result = 1;
+    for(let i = 1; i <= number; i++) {
+      result *= i;
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
