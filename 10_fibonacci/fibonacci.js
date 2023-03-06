d6b1515ba74fb4aeb311cfe3a1c596310dd27d4c
billyx86 <billymk777@gmail.com>
2023-03-06 20:41:08 +0000

    Complete 10_fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(member) {
+    if(member < 0) {
+        return "OOPS";
+    }
+    let n1 = 0, n2 = 1, next;
+    for(let i = 1; i <= member; i++) {
+        next = n1 + n2;
+        n1 = n2;
+        n2 = next;
+    }
+    return n1;
 };
 
 // Do not edit below this line
